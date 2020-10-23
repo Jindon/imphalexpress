72fa842e82ac6b086393e6bf3e729bc5316a6a8e
Jindon Moirangthem <jindon27@gmail.com>
2020-10-23 12:38:21 +0530

    fix close, delete button disabled states

--- resources/views/components/button/icon/close.blade.php
+++ resources/views/components/button/icon/close.blade.php
@@ -3,8 +3,12 @@
     'disabled' => false,
 ])
 
+@php
+    $checkDisabledClass = $disabled ? 'hover:bg-gray-300 cursor-not-allowed' : 'hover:bg-red-600 hover:text-red-100';
+@endphp
+
 <button
-    {{ $attributes->merge(['class' => 'flex items-center p-2 text-sm rounded transition duration-200 focus:outline-none text-red-600 ' . $disabled ? 'hover:bg-gray-300 cursor-not-allowed' : 'hover:bg-red-600 hover:text-red-100']) }}
+    {{ $attributes->merge(['class' => 'flex items-center p-2 text-sm rounded transition duration-200 focus:outline-none text-red-600 ' . $checkDisabledClass]) }}
     {{ $disabled ? 'disabled' : '' }}
 >
     <x-icon type="close" width="{{ $iconWidth }}"></x-icon>
