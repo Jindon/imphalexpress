6bcae8b3a828844d73f825fa97e1d0c4282a588b
Jindon Moirangthem <jindon27@gmail.com>
2020-10-23 12:25:35 +0530

    add superadmin seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,7 @@
 namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
 
 class DatabaseSeeder extends Seeder
 {
@@ -14,7 +15,11 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
          \App\Models\Settings::factory()->count(3)->create();
-         \App\Models\User::factory(1)->create();
+         \App\Models\User::factory()->create([
+             'role' => 'superadmin',
+             'email' => 'jindon27@gmail.com',
+             'password' => Hash::make('welcome27')
+         ]);
          \App\Models\Location::factory()->count(5)->create();
          \App\Models\Business::factory()->count(5)->create();
          \App\Models\Package::factory()->count(130)->create();
